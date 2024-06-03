b2e3bd40b89cb28098b316d6c9cb032c4403d54e
Callum Grove <callum.grove@ukaea.uk>
2024-06-03 17:09:31 +0100

    Docstring improvement.

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row
+contains inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -17,6 +17,7 @@ def load_csv(filename):
     :param filename: Filename of CSV to load
     """
     return np.loadtxt(fname=filename, delimiter=',')
+
 
 def load_json(filename):
     """Load a numpy array from a JSON document.
@@ -39,18 +40,28 @@ def load_json(filename):
         return [np.array(entry['observations']) for entry in data_as_json]
 
 
-
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: A 2D data array with inflammation data (each row contains
+    measurements for a single patient across all days).
+    :returns: An array of mean values of measurements for each day."""
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: A 2D data array with inflammation data (each row contains
+    measurements for a single patient across all days).
+    :returns: An array of max values of measurements for each day."""
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
-    return np.min(data, axis=0)
+    """Calculate the daily min of a 2D inflammation data array.
 
+    :param data: A 2D data array with inflammation data (each row contains
+    measurements for a single patient across all days).
+    :returns: An array of minimum values of measurements for each day."""
+    return np.min(data, axis=0)
